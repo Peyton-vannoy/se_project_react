bd34e0369896bae4fdae5825aee1f3ec0173dd77
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-06-20 17:21:43 -0400

    Footer added

--- vite-project/src/components/App/App.jsx
+++ vite-project/src/components/App/App.jsx
@@ -5,6 +5,7 @@ import Header from "../Header/Header";
 import Main from "../Main/Main";
 import ModalWithForm from "../ModalWithForm/ModalWithForm";
 import ItemModal from "../ItemModal/ItemModal";
+import Footer from "../Footer/Footer";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "hot" });
@@ -28,6 +29,7 @@ function App() {
       <div className="page__content">
         <Header handleAddClick={handleAddClick} />
         <Main weatherData={weatherData} handleCardClick={handleCardClick} />
+        <Footer />
       </div>
       <ModalWithForm
         buttonText="Add garment"
---
+++ vite-project/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer__text">Developed by: Peyton Vannoy</p>
+      <p className="footer__copyright"> 2024</p>
+    </footer>
+  );
+}
+
+export default Footer;
