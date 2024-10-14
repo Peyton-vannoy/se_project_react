83f35b4cf677266fbaf1d5525679289b9490eec9
Peyton Vannoy <peyton.vannoy1999@gmail.com>
2024-10-14 19:59:18 -0400

    Fix user able to see delete button

--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -36,7 +36,7 @@ function ItemModal({
           <div className="modal__footer">
             <h2 className="modal__caption">{card.name}</h2>
             <p className="modal__weather">Weather: {card.weather}</p>
-            {isLoggedIn && isOwn && (
+            {isLoggedIn && (
               <button
                 type="button"
                 className={itemDeleteButtonClassName}
